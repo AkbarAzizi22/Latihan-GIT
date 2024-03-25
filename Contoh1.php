9ed190d071a40c0b04f3d4891e6a83a2378b20a3
AkbarAzizi22 <163988301+AkbarAzizi22@users.noreply.github.com>
2024-03-25 14:31:43 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller{
+    public function index()
+    {
+        echo "<h1>Perkenaln</h1>";
+        echo "Nama Saya Akbar Falika Azizi 
+        Saya tinggal di daerah Bekasi 
+        kegiatan yang saya suka yaitu makan";
+    }
+}
